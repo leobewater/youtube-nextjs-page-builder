91d5f5a4977fd6ac3d8e9b256573a8eac3096a3b
leobewater <mmbee888@gmail.com>
2023-10-13 16:17:06 -0400

    set up the rest of the stats cards

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,9 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Suspense } from 'react';
 import { LuView } from 'react-icons/lu';
+import { FaWpforms } from 'react-icons/fa';
+import { HiCursorClick } from 'react-icons/hi';
+import { TbArrowBounce } from 'react-icons/tb';
 
 export default function Home() {
   return (
@@ -36,6 +39,33 @@ function StatsCards(props: StatsCardProps) {
         value={data?.visits.toLocaleString() || ''}
         loading={loading}
         className="shadow-md shadow-blue-600"
+      />
+
+      <StatsCard
+        title="Total submissions"
+        icon={<FaWpforms className="text-yellow-600" />}
+        helperText="All time form submissions"
+        value={data?.submissions.toLocaleString() || ''}
+        loading={loading}
+        className="shadow-md shadow-yellow-600"
+      />
+
+      <StatsCard
+        title="Submission rate"
+        icon={<HiCursorClick className="text-green-600" />}
+        helperText="Visits that result in form submission"
+        value={data?.submissionRate.toLocaleString() || ''}
+        loading={loading}
+        className="shadow-md shadow-green-600"
+      />
+
+      <StatsCard
+        title="Bounce rate"
+        icon={<TbArrowBounce className="text-red-600" />}
+        helperText="Visits that leaves without interactions"
+        value={data?.visits.toLocaleString() || ''}
+        loading={loading}
+        className="shadow-md shadow-red-600"
       />
     </div>
   );
