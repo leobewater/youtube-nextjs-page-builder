78d042e44f70d4545084358d116f46e05b90f6bc
leobewater <mmbee888@gmail.com>
2023-10-15 09:37:51 -0400

    assign new element id and show component on dropzone

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import React, { useState } from 'react';
 import DesignerSidebar from './DesignerSidebar';
-import { useDroppable } from '@dnd-kit/core';
+import { DragEndEvent, useDndMonitor, useDroppable } from '@dnd-kit/core';
 import { cn } from '@/lib/utils';
-import { FormElementInstance } from './FormElements';
 import useDesigner from './hooks/useDesigner';
+import {
+  ElementsType,
+  FormElementInstance,
+  FormElements,
+} from './FormElements';
+import { idGenerator } from '@/lib/idGenerator';
 
 const Designer = () => {
   const { elements, addElement } = useDesigner();
@@ -14,6 +18,32 @@ const Designer = () => {
     id: 'designer-drop-area',
     data: {
       isDesignerDropArea: true,
+    },
+  });
+
+  console.log('Elements', elements);
+
+  useDndMonitor({
+    onDragEnd: (event: DragEndEvent) => {
+      //console.log('Drag End', event);
+      const { active, over } = event;
+      if (!active || !over) return;
+
+      // check if event is a designer btn element
+      const isDesignerBtnElement = active.data?.current?.isDesignerBtnElement;
+      if (isDesignerBtnElement) {
+        const type = active.data?.current?.type;
+
+        // create a new instance of the element
+        const newElement = FormElements[type as ElementsType].construct(
+          idGenerator()
+        );
+
+        // add new element to context api
+        addElement(0, newElement);
+
+        console.log(newElement);
+      }
     },
   });
 
@@ -38,11 +68,24 @@ const Designer = () => {
               <div className="h-[120px] rounded-md bg-primary/20"></div>
             </div>
           )}
+
+          {elements.length > 0 && (
+            <div className="flex flex-col text-background w-full gap-2 p-4">
+              {elements.map((element) => (
+                <DesignerElementWrapper key={element.id} element={element} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <DesignerSidebar />
     </div>
   );
 };
+
+function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+  const DesignerElement = FormElements[element.type].designerComponent;
+  return <DesignerElement />;
+}
 
 export default Designer;
