8fdae883fc77f8b2ffceab6964f2387de9cfbbe9
leobewater <mmbee888@gmail.com>
2023-10-15 08:47:43 -0400

    set drag item logic

--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -1,13 +1,36 @@
-import { useDndMonitor } from '@dnd-kit/core';
+import { Active, DragOverlay, useDndMonitor } from '@dnd-kit/core';
+import { useState } from 'react';
+import { SidebarBtnElementOverlay } from './SidebarBtnElement';
+import { ElementsType, FormElements } from './FormElements';
 
 const DragOverlayWrapper = () => {
+  const [draggedItem, setDraggedItem] = useState<Active | null>(null);
+
   useDndMonitor({
     onDragStart: (event) => {
-      console.log('DRAG ITEM', event);
+      //console.log('DRAG ITEM', event);
+      setDraggedItem(event.active);
+    },
+    onDragCancel: () => {
+      setDraggedItem(null);
+    },
+    onDragEnd: () => {
+      setDraggedItem(null);
     },
   });
-    
-  return <div>DragOverlayWrapper</div>;
+
+  if (!draggedItem) return null;
+
+  let node = <div>No drag overlay</div>;
+  const isSidebarBtnElement = draggedItem?.data?.current?.isDesignerBtnElement;
+
+  // if element is sidebar btn element, then show the btn overlay component
+  if (isSidebarBtnElement) {
+    const type = draggedItem.data?.current?.type as ElementsType;
+    node = <SidebarBtnElementOverlay formElement={FormElements[type]} />;
+  }
+
+  return <DragOverlay>{node}</DragOverlay>;
 };
 
 export default DragOverlayWrapper;
