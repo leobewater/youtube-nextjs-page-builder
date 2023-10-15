7e057ebf1116dbf1f15f41a8032b30f94444dde6
leobewater <mmbee888@gmail.com>
2023-10-15 17:18:52 -0400

    add FormSubmitComponent

---
+++ components/FormSubmitComponent.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { FormElementInstance } from './FormElements';
+
+const FormSubmitComponent = ({
+  formUrl,
+  content,
+}: {
+  formUrl: string;
+  content: FormElementInstance[];
+}) => {
+  return <div>FormSubmitComponent</div>;
+};
+
+export default FormSubmitComponent;
