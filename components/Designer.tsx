054ef7a31a8813f42d3f9f9033655587e2b43555
leobewater <mmbee888@gmail.com>
2023-10-15 09:54:42 -0400

    set up dropping above or below styling

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -84,10 +84,39 @@ const Designer = () => {
 };
 
 function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+  const topHalf = useDroppable({
+    id: element.id + '-top',
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isTopHalfDesignerElement: true,
+    },
+  });
+
+  const bottomHalf = useDroppable({
+    id: element.id + '-bottom',
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isBottomHalfDesignerElement: true,
+    },
+  });
+
   const DesignerElement = FormElements[element.type].designerComponent;
   return (
-    <div className="flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2">
-      <DesignerElement elementInstance={element} />
+    <div className="relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset">
+      <div
+        ref={topHalf.setNodeRef}
+        className="absolute w-full h-1/2 rounded-t-md"
+      ></div>
+      <div
+        ref={bottomHalf.setNodeRef}
+        className="absolute  w-full bottom-0 h-1/2 rounded-b-md"
+      ></div>
+
+      <div className="flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none">
+        <DesignerElement elementInstance={element} />
+      </div>
     </div>
   );
 }
