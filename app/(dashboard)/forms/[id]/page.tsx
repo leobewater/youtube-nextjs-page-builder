1d516ccdeed4cf735853326e4d085e80a3b994d9
leobewater <mmbee888@gmail.com>
2023-10-15 16:58:10 -0400

    add visit form button to form detail page

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { GetFormById } from '@/actions/form';
 import FormBuilder from '@/components/FormBuilder';
+import VisitBtn from '@/components/VisitBtn';
 import React from 'react';
 
 async function BuilderPage({ params }: { params: { id: string } }) {
@@ -9,7 +10,25 @@ async function BuilderPage({ params }: { params: { id: string } }) {
     throw new Error('form not found');
   }
 
-  return <>Details</>;
+  const { visits, submissions } = form;
+
+  let submissionRate = 0;
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+
+  const bounceRate = 100 - submissionRate;
+
+  return (
+    <>
+      <div className="py-10 border-b border-muted">
+        <div className="flex justify-between container">
+          <h1 className="text-4xl font-bold truncate">{form.name}</h1>
+          <VisitBtn shareUrl={form.shareURL} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default BuilderPage;
