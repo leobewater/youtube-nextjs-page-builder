28c9288ea29c80b08106f903600b9fc94fd140d5
leobewater <mmbee888@gmail.com>
2023-10-15 09:49:03 -0400

    style element wrapper

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -85,7 +85,11 @@ const Designer = () => {
 
 function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
   const DesignerElement = FormElements[element.type].designerComponent;
-  return <DesignerElement elementInstance={element} />;
+  return (
+    <div className="flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2">
+      <DesignerElement elementInstance={element} />
+    </div>
+  );
 }
 
 export default Designer;
