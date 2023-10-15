f0ee9227db0cd28e8bc8e254375506be5dcac954
leobewater <mmbee888@gmail.com>
2023-10-15 12:21:12 -0400

    add loading spinner when loading form initially

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Form } from '@prisma/client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PreviewDialogBtn } from './PreviewDialogBtn';
 import SaveFormBtn from './SaveFormBtn';
 import PublishFormBtn from './PublishFormBtn';
@@ -14,8 +14,13 @@ import {
   useSensors,
 } from '@dnd-kit/core';
 import DragOverlayWrapper from './DragOverlayWrapper';
+import useDesigner from './hooks/useDesigner';
+import { ImSpinner2 } from 'react-icons/im';
 
 const FormBuilder = ({ form }: { form: Form }) => {
+  const { setElements } = useDesigner();
+  const [isReady, setIsReady] = useState(false);
+
   // fix delete button
   const mouseSensor = useSensor(MouseSensor, {
     activationConstraint: {
@@ -31,6 +36,21 @@ const FormBuilder = ({ form }: { form: Form }) => {
   });
 
   const sensors = useSensors(mouseSensor, touchSensor);
+
+  useEffect(() => {
+    const elements = JSON.parse(form.content);
+    setElements(elements);
+    const readyTimeout = setTimeout(() => setIsReady(true), 500);
+    return () => clearTimeout(readyTimeout);
+  }, [form, setElements]);
+
+  if (!isReady) {
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <ImSpinner2 className="animate-spin h-12 w-12" />
+      </div>
+    );
+  }
 
   return (
     <DndContext sensors={sensors}>
