4166bfafabcbaab6bf32762101744b5dbe0630fa
leobewater <mmbee888@gmail.com>
2023-10-15 12:02:54 -0400

    add update form server action

--- components/SaveFormBtn.tsx
+++ components/SaveFormBtn.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Button } from './ui/button';
 import { HiSaveAs } from 'react-icons/hi';
+import useDesigner from './hooks/useDesigner';
 
 const SaveFormBtn = () => {
+  const { elements } = useDesigner();
+
+  const updateFormContent = async () => {
+    try {
+      const JsonElements = JSON.stringify(elements);
+    } catch (error) {
+    }
+  };
+    
   return (
     <Button variant={'outline'} className="gap-2">
       <HiSaveAs className="h-4 w-4" />
