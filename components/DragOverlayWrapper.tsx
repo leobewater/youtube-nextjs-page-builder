9a7056e5fdd0bae16f272e068ff9708a876309b2
leobewater <mmbee888@gmail.com>
2023-10-15 10:09:16 -0400

    update style when dragging element

--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -2,8 +2,10 @@ import { Active, DragOverlay, useDndMonitor } from '@dnd-kit/core';
 import { useState } from 'react';
 import { SidebarBtnElementOverlay } from './SidebarBtnElement';
 import { ElementsType, FormElements } from './FormElements';
+import useDesigner from './hooks/useDesigner';
 
 const DragOverlayWrapper = () => {
+  const { elements } = useDesigner();
   const [draggedItem, setDraggedItem] = useState<Active | null>(null);
 
   useDndMonitor({
@@ -28,6 +30,24 @@ const DragOverlayWrapper = () => {
   if (isSidebarBtnElement) {
     const type = draggedItem.data?.current?.type as ElementsType;
     node = <SidebarBtnElementOverlay formElement={FormElements[type]} />;
+  }
+
+  // if element already dropped
+  const isDesignerElement = draggedItem.data?.current?.isDesignerElement;
+  if (isDesignerElement) {
+    const elementId = draggedItem.data?.current?.elementId;
+    // get the element from the context api
+    const element = elements.find((el) => el.id === elementId);
+    if (!element) node = <div>Element not found!</div>;
+    else {
+      const DesignerElementComponent =
+        FormElements[element.type].designerComponent;
+      node = (
+        <div className="flex bg-accent border rounded-md h-[120px] w-full py-2 px-4">
+          <DesignerElementComponent elementInstance={element} />
+        </div>
+      );
+    }
   }
 
   return <DragOverlay>{node}</DragOverlay>;
