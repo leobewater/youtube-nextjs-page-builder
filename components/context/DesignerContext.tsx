32385fa2b5793c888766ecbe26e04519cc0341bb
leobewater <mmbee888@gmail.com>
2023-10-15 11:17:19 -0400

    add update element in the designer context

--- components/context/DesignerContext.tsx
+++ components/context/DesignerContext.tsx
@@ -16,6 +16,8 @@ type DesignerContextType = {
 
   selectedElement: FormElementInstance | null;
   setSelectedElement: Dispatch<SetStateAction<FormElementInstance | null>>;
+
+  updateElement: (id: string, element: FormElementInstance) => void;
 };
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
@@ -41,14 +43,26 @@ export default function DesignerContextProvider({
     setElements((prev) => prev.filter((element) => element.id !== id));
   };
 
+  const updateElement = (id: string, element: FormElementInstance) => {
+    setElements((prev) => {
+      const newElements = [...prev];
+      const index = newElements.findIndex((el) => el.id === id);
+      newElements[index] = element;
+      return newElements;
+    });
+  };
+
   return (
     <DesignerContext.Provider
       value={{
         elements,
         addElement,
         removeElement,
+
         selectedElement,
         setSelectedElement,
+
+        updateElement,
       }}
     >
       {children}
