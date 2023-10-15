8be7c7048fb66899acfa7b330b5938245f53f174
leobewater <mmbee888@gmail.com>
2023-10-15 08:38:20 -0400

    set up textfield in sidebar

---
+++ components/FormElements.tsx
@@ -0,0 +1,36 @@
+import { TextFieldFormElement } from './fields/TextField';
+
+export type ElementsType = 'TextField';
+
+export type FormElement = {
+  type: ElementsType;
+
+  construct: (id: string) => FormElementInstance;
+
+  designerBtnElement: {
+    icon: React.ElementType;
+    label: string;
+  };
+
+  designerComponent: React.FC; // component showing on designer
+  formComponent: React.FC; // element form component
+  propertiesComponent: React.FC;
+};
+
+export type FormElementInstance = {
+  id: string;
+  type: ElementsType;
+  extraAttributes?: Record<string, any>;
+};
+
+// use ElementsType as array key
+type FormElementsType = {
+  [key in ElementsType]: FormElement;
+};
+
+export const FormElement: FormElementsType = {
+  TextField: TextFieldFormElement,
+};
+
+// Setting all these up so we can do this with type hint
+//FormElement['TextField'].designerBtnElement
