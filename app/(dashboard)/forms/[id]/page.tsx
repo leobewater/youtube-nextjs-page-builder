0885c5cf17c9b3ee1b0c044101b1ab4069dcc851
leobewater <mmbee888@gmail.com>
2023-10-15 17:10:52 -0400

    show form stats cards

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -1,7 +1,12 @@
 import { GetFormById } from '@/actions/form';
-import FormBuilder from '@/components/FormBuilder';
+import FormLinkShare from '@/components/FormLinkShare';
 import VisitBtn from '@/components/VisitBtn';
 import React from 'react';
+import { StatsCard } from '../../page';
+import { LuView } from 'react-icons/lu';
+import { FaWpforms } from 'react-icons/fa';
+import { HiCursorClick } from 'react-icons/hi';
+import { TbArrowBounce } from 'react-icons/tb';
 
 async function FormDetailPage({ params }: { params: { id: string } }) {
   const { id } = params;
@@ -27,8 +32,62 @@ async function FormDetailPage({ params }: { params: { id: string } }) {
           <VisitBtn shareUrl={form.shareURL} />
         </div>
       </div>
+      <div className="py-4 border-b border-muted">
+        <div className="container flex gap-2 items-center justify-between">
+          <FormLinkShare shareUrl={form.shareURL} />
+        </div>
+      </div>
+      <div className="w-full pt-8 gap-4 grid grid-col-1 md:grid-cols-2 lg:grid-cols-4 container">
+        <StatsCard
+          title="Total visits"
+          icon={<LuView className="text-blue-600" />}
+          helperText="All time form visits"
+          value={visits.toLocaleString() || ''}
+          loading={false}
+          className="shadow-md shadow-blue-600"
+        />
+
+        <StatsCard
+          title="Total submissions"
+          icon={<FaWpforms className="text-yellow-600" />}
+          helperText="All time form submissions"
+          value={submissions.toLocaleString() || ''}
+          loading={false}
+          className="shadow-md shadow-yellow-600"
+        />
+
+        <StatsCard
+          title="Submission rate"
+          icon={<HiCursorClick className="text-green-600" />}
+          helperText="Visits that result in form submission"
+          value={submissionRate.toLocaleString() + '%' || ''}
+          loading={false}
+          className="shadow-md shadow-green-600"
+        />
+
+        <StatsCard
+          title="Bounce rate"
+          icon={<TbArrowBounce className="text-red-600" />}
+          helperText="Visits that leaves without interactions"
+          value={visits.toLocaleString() + '%' || ''}
+          loading={false}
+          className="shadow-md shadow-red-600"
+        />
+      </div>
+
+      <div className="container pt-10">
+        <SubmissionTable id={form.id} />
+      </div>
     </>
   );
 }
 
 export default FormDetailPage;
+
+function SubmissionTable({ id }: { id: number }) {
+  return (
+    <>
+      <h1 className="text-2xl font-bold my-4">Submissions</h1>
+    </>
+  );
+}
