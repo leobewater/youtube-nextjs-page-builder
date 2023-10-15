8a2fb669a1bc846fb80b66f601c9683fdc4767af
leobewater <mmbee888@gmail.com>
2023-10-15 08:09:10 -0400

    set up form buttons

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -2,11 +2,14 @@
 
 import { Form } from '@prisma/client';
 import React from 'react';
+import { PreviewDialogBtn } from './PreviewDialogBtn';
+import SaveFormBtn from './SaveFormBtn';
+import PublishFormBtn from './PublishFormBtn';
 
 const FormBuilder = ({ form }: { form: Form }) => {
   return (
     <main className="flex flex-col w-full">
-      <div className="flex justify-between border-b-2 p-4 gap-3 items-center">
+      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
         <h2 className="truncate font-medium">
           <span className="text-muted-foreground mr-2">Form:</span>
           {form.name}
@@ -20,7 +23,8 @@ const FormBuilder = ({ form }: { form: Form }) => {
             </>
           )}
         </div>
-      </div>
+          </nav>
+          <div className=""></div>
     </main>
   );
 };
---
+++ components/PreviewDialogBtn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from './ui/button';
+import { MdPreview } from 'react-icons/md';
+
+export const PreviewDialogBtn = () => {
+  return (
+    <Button variant={'outline'} className="gap-2">
+      <MdPreview className="h-4 w-4" />
+      Preview
+    </Button>
+  );
+};
