117c3cc2df9b668611a1a9d8bdebe7795221313b
leobewater <mmbee888@gmail.com>
2023-10-15 08:51:18 -0400

    show droppable overlay and hide drop here label

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -23,9 +23,17 @@ const Designer = () => {
             droppable.isOver && 'ring-2 ring-primary'
           )}
         >
-          <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
-            Drop here
-          </p>
+          {!droppable.isOver && (
+            <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
+              Drop Here
+            </p>
+          )}
+
+          {droppable.isOver && (
+            <div className="p-4 w-full">
+              <div className="h-[120px] rounded-md bg-primary/20"></div>
+            </div>
+          )}
         </div>
       </div>
       <DesignerSidebar />
