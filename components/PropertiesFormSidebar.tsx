61060ac700a40a07475c5a5f96cbbd9eb0975297
leobewater <mmbee888@gmail.com>
2023-10-15 11:04:03 -0400

    add properties form close button

--- components/PropertiesFormSidebar.tsx
+++ components/PropertiesFormSidebar.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import useDesigner from './hooks/useDesigner';
 import { FormElements } from './FormElements';
+import { AiOutlineClose } from 'react-icons/ai';
+import { Button } from './ui/button';
 
 const PropertiesFormSidebar = () => {
-  const { selectedElement } = useDesigner();
+  const { selectedElement, setSelectedElement } = useDesigner();
   if (!selectedElement) return null;
 
   // get the properties form compoment from selected element
@@ -11,7 +13,17 @@ const PropertiesFormSidebar = () => {
     FormElements[selectedElement?.type].propertiesComponent;
 
   return (
-    <div>
+    <div className="flex flex-col p-2">
+      <div className="flex justify-between items-center">
+        <p className="text-sm text-foreground/70">Element Properties</p>
+        <Button
+          size={'icon'}
+          variant={'ghost'}
+          onClick={() => setSelectedElement(null)}
+        >
+          <AiOutlineClose />
+        </Button>
+      </div>
       <PropertiesForm />
     </div>
   );
